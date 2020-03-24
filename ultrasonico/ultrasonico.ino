605e8ef9353792e9a6f5b2ab21bbbf0ef6302d28
William Condori <46354411+WilliamAbelCondori@users.noreply.github.com>
2020-03-24 13:15:27 -0400

    Corrigiendo Laboratorio 1

--- ultrasonico/ultrasonico.ino
+++ ultrasonico/ultrasonico.ino
@@ -1,5 +1,5 @@
-int Echo = 4;
-int Trig = 2;
+int Echo = 11;
+int Trig = 12;
 char dato;
 
 int contador = 0;
@@ -8,30 +8,24 @@ int valor_acumulado = 0;
 int valor_real = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(Trig, OUTPUT);
   pinMode(Echo, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
   Serial.println("Laboratorio 1");
   Serial.println("ROBOTICA");
   Serial.println("-------------------------------");
   Serial.println("Enviando Trigger....");
-  Serial.println("Presione la tecla 'a' ");
-
+  Serial.println("Presione la tecla 'a' para comenzar ");
 
   do {
     dato = Serial.read();
 
   } while (dato != 'a');
-  //valor_acumulado=micros();
 
-  if (dato == 'a') {
-
+  
     long duracion;
     long distancia;
 
@@ -48,27 +42,13 @@ void loop() {
     contador2 = micros();
     duracion = contador2 - contador;
 
-
-
-
-    //duracion = pulseIn(Echo, HIGH);
     duracion = duracion / 2;
     distancia = duracion / 29; //con esto tenemos nuestra distancia en cm
-
-
 
     Serial.println((String)"Pulso Echo recibido. ");
     Serial.println((String)"Tiempo = " + duracion + " us.");
     Serial.println((String)"Distancia: " + distancia + " cm.");
-    Serial.println("");
-    Serial.println("");
-    Serial.println("");
-    Serial.println("");
-    Serial.println("");
-
-
-  }
-  delay(50);
+    delay(50);
 
 
 }
